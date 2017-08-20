3dcf5bf83aebc2d2f4f0e1cf4d64387143a7973b
ErAnup <patilanup20@gmail.com>
2017-08-20 13:27:44 +0530

    First Commit

---
+++ app/src/main/java/com/intentexample1/FirstActivity.java
@@ -0,0 +1,28 @@
+package com.intentexample1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),AnotherActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
